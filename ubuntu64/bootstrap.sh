30480f66700f8aa7885ff44266986ad2fc7a1c23
nugmeg <liu.ron@husky.neu.edu>
2018-03-07 16:48:05 -0500

    gear up

--- ubuntu64/bootstrap.sh
+++ ubuntu64/bootstrap.sh
@@ -2,8 +2,14 @@
 echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
 
 # get necessary packages
-apt-get install -y gcc gdb make git python-pip curl radare2
+apt-get install -y gcc gdb make git python-pip curl radare2 nasm vim
 pip install ropper
 
+# install victim scripts
+rm -rf /home/vagrant/buffer_overruns
 git clone --depth=1 https://github.com/ISSA-NU/buffer_overruns.git /home/vagrant/buffer_overruns
 chown -R vagrant:vagrant /home/vagrant/buffer_overruns
+
+# gear up vim
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh /home/vagrant/.vim_runtime/install_awesome_vimrc.sh
