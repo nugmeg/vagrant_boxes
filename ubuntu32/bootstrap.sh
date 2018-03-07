fc9fb2a79627dfaddfb54a5ba1d00bb1e655260b
nugmeg <liu.ron@husky.neu.edu>
2018-03-07 13:34:18 -0500

    update bootstrap

--- ubuntu32/bootstrap.sh
+++ ubuntu32/bootstrap.sh
@@ -2,18 +2,11 @@
 echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
 
 # get necessary packages
-apt-get install -y gcc make git python-pip curl
-git clone --depth=1 https://github.com/radare/radare2.git ~/radare2
+apt-get install -y gcc gdb make git python3-pip curl radare2
+pip3 install ropper
 
-# install radare2
-cd ~/radare2
-sys/install.sh
-cd ~
-
-# install ropper
-pip install capstone
-pip install filebytes
-pip install keystone-engine
-
+# install victim scripts
+git clone --depth=1 https://github.com/ISSA-NU/buffer_overruns.git /home/vagrant/buffer_overruns
+chown -R vagrant:vagrant /home/vagrant/buffer_overruns
 
 
--- ubuntu64/bootstrap.sh
+++ ubuntu64/bootstrap.sh
@@ -2,5 +2,8 @@
 echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
 
 # get necessary packages
-apt-get install -y gcc make git python-pip curl radare2
+apt-get install -y gcc gdb make git python-pip curl radare2
 pip install ropper
+
+git clone --depth=1 https://github.com/ISSA-NU/buffer_overruns.git /home/vagrant/buffer_overruns
+chown -R vagrant:vagrant /home/vagrant/buffer_overruns
