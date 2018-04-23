74d8345cbe3d5dd8c8b84b07c68979e6637582ed
nutmeg <37119527+nugmeg@users.noreply.github.com>
2018-04-23 12:01:45 -0400

    Update bootstrap.sh

--- ubuntu64/bootstrap.sh
+++ ubuntu64/bootstrap.sh
@@ -7,7 +7,7 @@ pip install ropper
 
 # install victim scripts
 rm -rf /home/vagrant/buffer_overruns
-git clone --depth=1 https://github.com/ISSA-NU/buffer_overruns.git /home/vagrant/buffer_overruns
+git clone --depth=1 https://github.com/nugmeg/buffer_overruns.git /home/vagrant/buffer_overruns
 chown -R vagrant:vagrant /home/vagrant/buffer_overruns
 
 # gear up vim
